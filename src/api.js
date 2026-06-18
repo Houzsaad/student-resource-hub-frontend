297b46ac92f079603a4a559d63b71108bbaa3b55
Houzsaad <227036578+Houzsaad@users.noreply.github.com>
2026-06-17 23:40:21 -0900

    fix: use BASE for register

--- src/api.js
+++ src/api.js
@@ -34,7 +34,7 @@ export async function loginUser(email, password) {
 
 
 export async function registerUser(formData) {
-    const res = await fetch(`${BASE}/accounts/register/`, {
+    const res = await fetch(`${BASE}/auth/users/`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(formData),
