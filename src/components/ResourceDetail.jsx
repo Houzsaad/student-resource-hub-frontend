7f3de4fb32013523e403622d531454fae6bd8fb8
Houzsaad <227036578+Houzsaad@users.noreply.github.com>
2026-08-04 16:50:04 -0700

    re-change the logic of opening pdf way

--- src/components/ResourceDetail.jsx
+++ src/components/ResourceDetail.jsx
@@ -28,9 +28,7 @@ function ResourceDetail() {
   const isPDF = resource.resource_type === "pdf"
 
   const fileUrl = resource.file
-    ? isPDF 
-      ? resource.file.replace("/image/upload/", "/raw/upload/fl_inline/")
-      : resource.file.replace("/image/upload/", "/raw/upload")
+    ? resource.file.replace("/image/upload/", "/raw/upload/fl_inline")
     : null;
     
   return (
