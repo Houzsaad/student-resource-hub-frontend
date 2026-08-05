0d09a85899c335a1aa4d0b634bf8d7f951fe4532
Houzsaad <227036578+Houzsaad@users.noreply.github.com>
2026-08-04 17:08:06 -0700

    enforce browser to save file internally

--- src/components/ResourceDetail.jsx
+++ src/components/ResourceDetail.jsx
@@ -28,7 +28,9 @@ function ResourceDetail() {
   const isPDF = resource.resource_type === "pdf"
 
   const fileUrl = resource.file
-    ? resource.file.replace("/image/upload/", "/raw/upload/")
+   ? isPDF 
+      ? resource.file.replace("/image/upload/", "/raw/upload/fl_inline/")
+      : resource.file.replace("/image/upload/", "/raw/upload/")
     : null;
     
   return (
